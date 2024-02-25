fb53f3517a05380d9cddc37d0bda2765786f9133
kenandotfyi <kenan.akinci@icloud.com>
2024-02-25 19:02:17 +0100

    added support for syntax highlighting and LaTeX formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,26 @@
 import { defineConfig } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
+    remarkPlugins: [remarkMath],
     rehypePlugins: [
       [
         rehypeExternalLinks,
         {
           content: { type: "text", value: "↗" },
+          target: "_blank",
         },
       ],
+      [rehypeKatex, {}],
     ],
+    shikiConfig: {
+      theme: "vitesse-dark",
+      wrap: true,
+    },
   },
   site: "https://kenan.fyi",
   integrations: [],
