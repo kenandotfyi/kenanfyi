88491601c3cbd1a65c09cb3ed6c13e03955686f8
kenandotfyi <kenan.akinci@icloud.com>
2024-02-25 17:52:14 +0100

    some style update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from "astro/config";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          content: { type: "text", value: "↗" },
+        },
+      ],
+    ],
+  },
   site: "https://kenan.fyi",
   integrations: [],
 });
