946fdbe34d1234bd8579d0b74e2ddc93af89dfc1
kenandotfyi <kenan.akinci@icloud.com>
2024-04-19 13:27:03 +0200

    adding full content to rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,8 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 // import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-const parser = new MarkdownIt();
+// import MarkdownIt from "markdown-it";
+// const parser = new MarkdownIt();
 
 export async function GET(context: any) {
   let articles = await getCollection("articles", ({ data }) => {
