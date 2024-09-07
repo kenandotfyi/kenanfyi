8fc19e906e8ab73349d1a4143333c5a183ffed19
kenandotfyi <kenan.akinci@icloud.com>
2024-09-07 21:12:38 +0200

    added syntax highlighting options and self hosted fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,16 @@ import rehypeExternalLinks from "rehype-external-links";
 import rehypeKatex from "rehype-katex";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 import remarkMath from "remark-math";
+import {
+  transformerNotationDiff,
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+  transformerNotationFocus,
+  transformerNotationErrorLevel,
+  transformerMetaHighlight,
+  transformerMetaWordHighlight,
+  transformerRenderWhitespace,
+} from '@shikijs/transformers';
 
 import mdx from "@astrojs/mdx";
 
@@ -22,9 +32,18 @@ export default defineConfig({
       ],
       [rehypeKatex, {}],
     ],
+    syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: "vitesse-light",
       wrap: false,
+      transformers: [transformerNotationDiff(),
+      transformerNotationHighlight(),
+      transformerNotationWordHighlight(),
+      transformerNotationFocus(),
+      transformerNotationErrorLevel(),
+      transformerMetaHighlight(),
+      transformerMetaWordHighlight(),
+      transformerRenderWhitespace(),]
     },
   },
   site: "https://kenan.fyi",
