47ddbbea9a959418ccfd05ccebd895d55513f6de
kenandotfyi <kenan.akinci@icloud.com>
2024-10-27 19:39:14 +0100

    fixed mobile menu

--- src/scripts/mobile-menu.js
+++ src/scripts/mobile-menu.js
@@ -2,17 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const floatingButton = document.getElementById('floatingButton');
   const menu = document.getElementById('menu');
 
-  floatingButton.innerHTML = "◀︎";
+  floatingButton.innerHTML = "☰";
+  floatingButton.style.fontSize = '1rem';
 
   floatingButton.addEventListener('click', () => {
     // Toggle the display of the menu
     if (menu.style.display === 'none' || menu.style.display === '') {
-      floatingButton.innerHTML = "▶︎";
+      floatingButton.innerHTML = "×";
       menu.style.display = 'flex';
+      floatingButton.style.fontSize = '20px';
       menu.style.flexDirection = 'row';
     } else {
       menu.style.display = 'none';
-      floatingButton.innerHTML = "◀︎";
+      floatingButton.innerHTML = "☰";
+      floatingButton.style.fontSize = '1rem';
     }
   });
 
